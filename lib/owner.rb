c0bc3a4893561f1a2f2f1279aa35fd1706a78e14
adam-larosa <adam.larosa@flatironschool.com>
2020-08-26 22:20:11 -0700

    lol

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
+require  "pry"
+
 class Owner
-  # code goes here
+    attr_reader :name, :species
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @species = "human"
+        @@all << self
+    end
+
+    def say_species
+        return "I am a #{@species}."
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.count
+        @@all.size
+    end
+
+    def self.reset_all
+        @@all = []
+    end
+
+    def cats
+
+    end
 end
