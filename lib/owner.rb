4a7410e50b2c55d13c531477ffa38a699cefded4
adam-larosa <adam.larosa@flatironschool.com>
2020-08-26 22:43:28 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,6 +27,43 @@ class Owner
     end
 
     def cats
+        Cat.all.select {|cat| cat.owner == self}
+    end
 
+    def dogs
+        Dog.all.select {|dog| dog.owner == self}
+    end
+
+    def buy_cat(cat)
+        Cat.new(cat, self)
+    end
+
+    def buy_dog(dog)
+        Dog.new(dog, self)
+    end
+
+    def walk_dogs
+        self.dogs.each {|dog| dog.mood = "happy"}
+    end
+
+    def feed_cats
+        self.cats.each {|cat| cat.mood = "happy"}
+    end
+
+    def sell_pets
+        self.dogs.each {|dog|
+            dog.mood = "nervous"
+            dog.owner = nil
+        }
+        self.cats.each {|cat| 
+            cat.mood = "nervous"
+            cat.owner = nil
+        }
+    end
+
+    def list_pets
+        dogs = self.dogs.count
+        cats = self.cats.count
+        return "I have #{dogs} dog#{dogs > 1 && "(s)"}, and #{cats} cat#{cats > 1 && "(s)"}."
     end
 end
